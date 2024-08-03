fa83414fabc404bb59f14ea1ebb2596e33cc8d70
Shiva Kumar Sahoo <codewithshivakumar@gmail.com>
2024-08-04 02:03:17 +0530

    basic profile screen setup

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-1 mt-5">
+      <View className="flex items-center justify-center py-5">
+        <Text className="text-xl font-bold">Profile</Text>
+      </View>
+      <View className="flex flex-row items-center gap-2 px-2 py-4 bg-blue-400">
+        <View className="">
+          <Image
+            source={require("../assets/profile.jpeg")}
+            className="w-20 h-20 rounded-full"
+          />
+        </View>
+        <View className="flex flex-row gap-2 items-center">
+          <View className="flex flex-row gap-1">
+            <Text className="font-semibold text-sm text-white">First Name</Text>
+            <Text className="font-semibold text-sm text-white">Last Name</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
