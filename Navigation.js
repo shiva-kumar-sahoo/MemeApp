307daf4aae995f1cb56e17a99f6e9c652e13348e
Shiva Kumar Sahoo <codewithshivakumar@gmail.com>
2024-08-03 09:46:07 +0530

    Set Screen Icons

--- Navigation.js
+++ Navigation.js
@@ -2,14 +2,31 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Entypo size={28} name="home" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" size={28} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
