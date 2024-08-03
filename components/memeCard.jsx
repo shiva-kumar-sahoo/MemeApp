5834df44b7780c53a4d512d79bd28be166670ee0
Shiva Kumar Sahoo <codewithshivakumar@gmail.com>
2024-08-03 23:16:32 +0530

    display MemeCards

--- components/memeCard.jsx
+++ components/memeCard.jsx
@@ -1,29 +1,43 @@
 import { View, Text, Image } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, AntDesign, Ionicons } from "@expo/vector-icons";
 
-const memeCard = () => {
-  const memeImage = require("../assets/demo.jpg");
+const memeCard = ({ item }) => {
+  const memeImage = item?.preview
+    ? { uri: item?.preview[1] }
+    : require("../assets/demo.jpg");
+  console.log(item?.preview[1]);
+
   return (
-    <View>
+    <View className="p-4 my-4 border">
       <View className="flex flex-row justify-between items-center">
-        <View>
-          <Text>Author Name</Text>
+        <View className="flex flex-row items-center gap-2">
+          <View className="mx-5 w-10 h-10 rounded-full">
+            <Image
+              source={require("../assets/profile.jpeg")}
+              className="w-10 h-10 rounded-full ml-4 "
+            />
+          </View>
+          <Text>{item?.author}</Text>
         </View>
         <View>
           <Entypo name="dots-three-vertical" size={24} color="black" />
         </View>
       </View>
-      <View className="flex items-center p-2">
-        <Text>Title</Text>
+      <View className="flex p-2">
+        <Text>{item?.title}</Text>
       </View>
-      <View>
+      <View className="flex items-center my-5">
         <Image
           source={memeImage}
-          className=""
-          placeholder={{ blurhash }}
+          className="w-60 h-40"
           contentFit="cover"
           transition={1000}
         />
+      </View>
+      <View className="flex flex-row justify-between">
+        <AntDesign name="hearto" size={24} color="black" />
+        <AntDesign name="sharealt" size={24} color="black" />
+        <Ionicons name="bookmark-outline" size={24} color="black" />
       </View>
     </View>
   );
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlashList } from "@shopify/flash-list";
+import getMeme from "../lib/getMeme";
+import MemeCard from "../components/memeCard";
 
 const Home = () => {
+  const [memeData, setMemeData] = useState([]);
+
+  useEffect(() => {
+    const fetchMemeData = async () => {
+      const data = await getMeme();
+      setMemeData(data);
+    };
+
+    fetchMemeData();
+  }, []);
+
   return (
-    <View className="flex-1">
-      <Text>Home</Text>
+    <View className="flex-1 mt-5">
+      <View className="px-4">
+        <FlatList
+          renderItem={({ item }) => {
+            return <MemeCard item={item} />;
+          }}
+          estimatedItemSize={50}
+          data={memeData}
+        />
+      </View>
     </View>
   );
 };
