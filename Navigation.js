fb30a38cb5b5440e8d55996ad5c2500ece6f26f2
Shiva Kumar Sahoo <codewithshivakumar@gmail.com>
2024-08-05 19:50:38 +0530

    solve same name navigation of Profile

--- Navigation.js
+++ Navigation.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 function SavedMeme() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="ProfileScreen" component={Profile} />
       <Stack.Screen name="ViewSavedMeme" component={ViewSavedMeme} />
     </Stack.Navigator>
   );
